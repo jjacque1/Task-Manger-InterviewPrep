0f27dd3537abf4c9f332dc8dd441074108b6be54
jjacque1 <jacksonjacque.jj@gmail.com>
2026-02-19 15:10:05 -0500

    created a addTask function inside app.jsx to define the shape of each task that will be added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,25 @@
+import { useState } from "react"
+
+const STATUS = {
+  PENDING: "pending",
+  In_PROGRESS: "in-progress",
+  Done : "done", 
+}
+
+function addTask(title) {
+  const newTask = {
+    id: Date.now(),
+    title: title,
+    status : STATUS.PENDING,
+  }
+
+  setTasks((prevTask) => [...prevTask, newTask])
+}
+
 export default function App() {
+
+  const [tasks, setTasks] = useState([])
+
   return (
     <div>
       <h1>Task manager App</h1>
